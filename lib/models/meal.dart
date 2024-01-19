9a80b22a59eed4fe7141decc5e7f4aab5e105a06
Sarthak <sarthak.00puri@gmail.com>
2024-01-19 17:42:28 +0530

    added meal DataModel

---
+++ lib/models/meal.dart
@@ -0,0 +1,48 @@
+
+enum Affordability{ Cheap, Affordable, Expensive }
+enum Complexity{Easy, Moderate, Hard}
+
+// enum Complexity {
+//   simple,
+//   challenging,
+//   hard,
+// }
+
+// enum Affordability {
+//   affordable,
+//   pricey,
+//   luxurious,
+// }
+class Meal{
+  const Meal({required this.id,
+  required this.title,
+  required this.categories,
+  required this.imageUrl,
+  required this.ingredients,
+  required this.steps,
+  required this.duration,
+  required this.isGlutenFree,
+  required this.isLactoseFree,
+  required this.isVegan,
+  required this.isVegetarian,
+  required this.affordability,
+  required this.complexity
+  });
+
+
+
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Affordability affordability;
+  final Complexity complexity;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+}
