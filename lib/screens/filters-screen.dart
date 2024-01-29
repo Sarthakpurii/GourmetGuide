cfe5d93a562dab6da7216d70b89d5d018552c233
Sarthak <sarthak.00puri@gmail.com>
2024-01-29 22:59:32 +0530

    updated filters screen

--- lib/screens/filters-screen.dart
+++ lib/screens/filters-screen.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 
+enum Filter {GluttenFree, LactoseFree,Vegetarian,Vegan}
+
 class FilterScreen extends StatefulWidget{
   FilterScreen({super.key});
 
@@ -20,46 +22,58 @@ class _FilterScreenState extends State<FilterScreen>{
   Widget build(context){
     return Scaffold(
       appBar: AppBar(title: Text('Available Filters'),),
-      body: Column(
-        children: [
-          SwitchListTile(value: _isGluttenFree,
-            onChanged: (isChecked){
-              setState(() {
-                _isGluttenFree=isChecked;
-              });
-            },
-            title: Text('Glutten-Free',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
-            subtitle: Text('Only include Glutten-Free recipes',style: TextStyle(color: Colors.white),),),
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop({
+            Filter.GluttenFree:_isGluttenFree,
+            Filter.LactoseFree: _isLactoseFree,
+            Filter.Vegan: _isVeg,
+            Filter.Vegetarian: _isVegan
+          });
+          return false;
+        },
 
-          SwitchListTile(value: _isLactoseFree,
-            onChanged: (isChecked){
-              setState(() {
-                _isLactoseFree=isChecked;
-              });
-            },
-            title: Text('Lactose-Free',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
-            subtitle: Text('Only include Lactose-Free recipes',style: TextStyle(color: Colors.white),),),
-
-
-
-          SwitchListTile(value: _isVeg,
-            onChanged: (isChecked){
-              setState(() {
-                _isVeg=isChecked;
-              });
-            },
-            title: Text('Vegetarian',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
-            subtitle: Text('Only include Vegetarian recipes',style: TextStyle(color: Colors.white),),),
-
-          SwitchListTile(value: _isVegan,
-            onChanged: (isChecked){
-              setState(() {
-                _isVegan=isChecked;
-              });
-            },
-            title: Text('Vegan',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
-            subtitle: Text('Only include Vegan recipes',style: TextStyle(color: Colors.white),),),
-        ],
+        child: Column(
+          children: [
+            SwitchListTile(value: _isGluttenFree,
+              onChanged: (isChecked){
+                setState(() {
+                  _isGluttenFree=isChecked;
+                });
+              },
+              title: Text('Glutten-Free',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
+              subtitle: Text('Only include Glutten-Free recipes',style: TextStyle(color: Colors.white),),),
+        
+            SwitchListTile(value: _isLactoseFree,
+              onChanged: (isChecked){
+                setState(() {
+                  _isLactoseFree=isChecked;
+                });
+              },
+              title: Text('Lactose-Free',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
+              subtitle: Text('Only include Lactose-Free recipes',style: TextStyle(color: Colors.white),),),
+        
+        
+        
+            SwitchListTile(value: _isVeg,
+              onChanged: (isChecked){
+                setState(() {
+                  _isVeg=isChecked;
+                });
+              },
+              title: Text('Vegetarian',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
+              subtitle: Text('Only include Vegetarian recipes',style: TextStyle(color: Colors.white),),),
+        
+            SwitchListTile(value: _isVegan,
+              onChanged: (isChecked){
+                setState(() {
+                  _isVegan=isChecked;
+                });
+              },
+              title: Text('Vegan',style: TextStyle(color: Color.fromARGB(255, 240, 95, 73)),),
+              subtitle: Text('Only include Vegan recipes',style: TextStyle(color: Colors.white),),),
+          ],
+        ),
       )
     );
   }
